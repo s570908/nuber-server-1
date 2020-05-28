42aab4101b06560f1f7f08bc000d0003ccd62592
kko0831 <kko0831@hanmail.net>
2020-05-28 17:20:42 +0900

    #1.13 User Entity part One

---
+++ src/entities/User.ts
@@ -0,0 +1,48 @@
+import { IsEmail } from "class-validator";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text", nullable: true })
+  @IsEmail()
+  email: string | null;
+
+  @Column({ type: "boolean", default: false })
+  verifiedEmail: boolean;
+
+  @Column({ type: "text" })
+  firstName: string;
+
+  @Column({ type: "text" })
+  lasttName: string;
+
+  @Column({ type: "int" })
+  age: number;
+
+  @Column({ type: "text" })
+  password: string;
+
+  @Column({ type: "text" })
+  phoneNumber: string;
+
+  @Column({ type: "boolean", default: false })
+  verifiedPhoneNumber: boolean;
+
+  @Column({ type: "text" })
+  profilePhoto: string;
+
+  @CreateDateColumn() createAt: string;
+
+  @UpdateDateColumn() updateAt: string;
+}
+
+export default User;
