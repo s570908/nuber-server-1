495e5eb784d02d01004eb3d06462aeff3068c8a3
kko0831 <kko0831@hanmail.net>
2020-05-29 19:02:45 +0900

    #1.21 Ride Entity

---
+++ src/entities/Ride.ts
@@ -0,0 +1,52 @@
+import { rideStatus } from "src/types/types";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+class Ride extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({
+    type: "text",
+    enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
+  })
+  status: rideStatus;
+
+  @Column({ type: "text" })
+  pickUpAddress: string;
+
+  @Column({ type: "double precision", default: 0 })
+  pickUpLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  pickUpLng: number;
+
+  @Column({ type: "text" })
+  dropOffAddress: string;
+
+  @Column({ type: "double precision", default: 0 })
+  dropOffLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  dropOffLng: number;
+
+  @Column({ type: "double precision" })
+  price: number;
+
+  @Column({ type: "text" })
+  distance: string;
+
+  @Column({ type: "text" })
+  duration: string;
+
+  @CreateDateColumn() createAt: string;
+  @UpdateDateColumn() updateAt: string;
+}
+
+export default Ride;
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,1 +1,7 @@
 export type verificationTarget = "EMAIL" | "PHONE";
+export type rideStatus =
+  | "ACCEPTED"
+  | "FINISHED"
+  | "CANCELED"
+  | "REQUESTING"
+  | "ONROUTE";
