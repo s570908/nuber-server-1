8e8c0410baf34cf20eed31a50de9657cdc5cc799
kko0831 <kko0831@hanmail.net>
2020-05-29 18:35:52 +0900

    #1.20 Place Entity

---
+++ src/entities/Place.ts
@@ -0,0 +1,33 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+class Place extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text" })
+  name: string;
+
+  @Column({ type: "double precision", default: 0 })
+  lat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lng: number;
+
+  @Column({ type: "text" })
+  address: string;
+
+  @Column({ type: "boolean" })
+  isFav: boolean;
+
+  @CreateDateColumn() createAt: string;
+  @UpdateDateColumn() updateAt: string;
+}
+
+export default Place;
