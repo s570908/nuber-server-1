8e7e4e82228f136acb685be562810d80e80fda14
kko0831 <kko0831@hanmail.net>
2020-06-04 19:00:19 +0900

    #1.50 Sending Confirmation Email part Two

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -6,3 +6,19 @@ const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || "",
   domain: "sandboxa6cfa532199f4e4abb76ccc33d474d37.mailgun.org",
 });
+
+const sendEmail = (subject: string, html: string) => {
+  const emailData = {
+    from: "kko0831@hanmail.net", // 본인의 이메일로 변경
+    to: "kko0831@hanmail.net", // 본인의 이메일로 변경
+    subject,
+    html,
+  };
+  return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+  const emailSubject = `Hello~ ${fullName}, please verify your email`;
+  const emailBody = `Verify your email by clicking <a href="http://number.com/verification/${key}/">here</a>`;
+  return sendEmail(emailSubject, emailBody);
+};
