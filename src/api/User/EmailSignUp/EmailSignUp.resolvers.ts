fdbf51d167ca7e7521ac51073587e7d94ae85e1f
kko0831 <kko0831@hanmail.net>
2020-06-04 22:12:27 +0900

    #1.51 Sending Confirmation Email part Three

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -1,7 +1,9 @@
 import { EmailSignUpMutationArgs, EmailSignUpResponse } from "src/types/graph";
 import { Resolvers } from "src/types/resolvers";
 import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
 import createJWT from "../../../utils/createJWT";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -19,13 +21,35 @@ const resolvers: Resolvers = {
             token: null,
           };
         } else {
-          const newUser = await User.create({ ...args }).save();
-          const token = createJWT(newUser.id);
-          return {
-            ok: true,
-            error: null,
-            token,
-          };
+          const phoneVerification = await Verification.findOne({
+            payload: args.phoneNumber,
+            verified: true,
+          });
+          if (phoneVerification) {
+            const newUser = await User.create({ ...args }).save();
+            if (newUser.email) {
+              const emailVerification = await Verification.create({
+                payload: newUser.email,
+                target: "EMAIL",
+              }).save();
+              await sendVerificationEmail(
+                newUser.fullName,
+                emailVerification.key
+              );
+            }
+            const token = createJWT(newUser.id);
+            return {
+              ok: true,
+              error: null,
+              token,
+            };
+          } else {
+            return {
+              ok: false,
+              error: "You haven't verified your phone number",
+              token: null,
+            };
+          }
         }
       } catch (error) {
         return {
