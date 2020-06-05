72d31efe58e47c1b17d02370c2c2527e6375e597
kko0831 <kko0831@hanmail.net>
2020-06-05 18:58:54 +0900

    #1.52 Testing Email Sending

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,13 +4,13 @@ import Mailgun from "mailgun-js";
 
 const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || "",
-  domain: "sandboxa6cfa532199f4e4abb76ccc33d474d37.mailgun.org",
+  domain: "sandbox8887722127a442c5ac0167de6a1d502c.mailgun.org",
 });
 
 const sendEmail = (subject: string, html: string) => {
   const emailData = {
-    from: "kko0831@hanmail.net", // 본인의 이메일로 변경
-    to: "kko0831@hanmail.net", // 본인의 이메일로 변경
+    from: "2130231401@naver.com", // 본인의 이메일로 변경
+    to: "2130231401@naver.com", // 본인의 이메일로 변경
     subject,
     html,
   };
