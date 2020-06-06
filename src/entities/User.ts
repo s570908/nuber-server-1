324f0543d44835ff7eef2053112b4918de010248
kko0831 <kko0831@hanmail.net>
2020-06-06 14:07:55 +0900

    #1.60 AddPlace Resolver

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -14,6 +14,7 @@ import {
 } from "typeorm";
 import Chat from "./Chat";
 import Message from "./Message";
+import Place from "./Place";
 import Ride from "./Ride";
 
 const BCRYPT_ROUNDS = 10;
@@ -61,6 +62,9 @@ class User extends BaseEntity {
 
   @OneToMany((type) => Ride, (ride) => ride.driver)
   ridesAsDriver: Ride[];
+
+  @OneToMany((type) => Place, (place) => place.user)
+  places: Place[];
 
   @Column({ type: "boolean", default: false })
   isDriving: boolean;
