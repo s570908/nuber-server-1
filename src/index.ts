dbd37106535b3b4fe945a76e4690a5f5ed7e87d8
kko0831 <kko0831@hanmail.net>
2020-06-07 01:54:47 +0900

    #1.68 Authenticating WebSocket Subscriptions part One

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,18 @@ import ConnectionOptions from "./ormConfig";
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT: string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
+const SUBSCRIPTION_ENDPOINT: string = "/subscription";
 
 const appOptions: Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
   endpoint: GRAPHQL_ENDPOINT,
+  subscriptions: {
+    path: SUBSCRIPTION_ENDPOINT,
+    onConnect: async (connectionParams) => {
+      console.log(connectionParams);
+    },
+  },
 };
 
 const handleAppStat = () => console.log(`Listening on port ${PORT}`);
