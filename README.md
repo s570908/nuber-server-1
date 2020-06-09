7b5e468fe6a38d85811aefc345361c570ae45795
kko0831 <kko0831@hanmail.net>
2020-06-10 02:35:18 +0900

    #1.76 UpdateRideStatus Resolver part One + #1.77 UpdateRideStatus Resolver part Two

--- README.md
+++ README.md
@@ -33,9 +33,11 @@ Server for the (N)Uber Clone Course on Nomad Academy. GraphQL, Typescript, NodeJ
 - [x] Get My Places
 - [x] See Nearby Drivers
 - [x] Subscribe to Nearby Drivers
-- [ ] Request a Ride
-- [ ] Get Nearby Rides
-- [ ] Subscribe to Nearby Ride Requests
+- [x] Request a Ride
+- [x] Get Nearby Ride Requests
+- [x] Subscribe to Nearby Ride Requests
+- [x] Update Ride Status
+- [ ] Get Ride
 - [ ] Subscribe to Ride Status
 - [ ] Get Chat Room Messages
 - [ ] Subscribe to Chat Room Messages
---
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -0,0 +1,68 @@
+import {
+  UpdateRideStatusMutationArgs,
+  UpdateRideStatusResponse,
+} from "src/types/graph";
+import { Resolvers } from "src/types/resolvers";
+import Ride from "../../../entities/Ride";
+import User from "../../../entities/User";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateRideStatus: privateResolver(
+      async (
+        _,
+        args: UpdateRideStatusMutationArgs,
+        { req }
+      ): Promise<UpdateRideStatusResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          try {
+            let ride: Ride | undefined;
+            if (args.status === "ACCEPTED") {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                status: "REQUESTING",
+              });
+              if (ride) {
+                ride.driver = user;
+                user.isTaken = true;
+                user.save();
+              }
+            } else {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                driver: user,
+              });
+            }
+            if (ride) {
+              ride.status = args.status;
+              ride.save();
+              return {
+                ok: true,
+                error: null,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Can't found Ride",
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "User is Not on driving",
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
