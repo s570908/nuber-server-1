8a5f1c499e8fcb4dadfdd12c9161e1afde89fe32
kko0831 <kko0831@hanmail.net>
2020-06-11 00:40:10 +0900

    #1.84 Testing GetChat Resolver

--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -10,9 +10,12 @@ const resolvers: Resolvers = {
       async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
         const user: User = req.user;
         try {
-          const chat = await Chat.findOne({
-            id: args.chatId,
-          });
+          const chat = await Chat.findOne(
+            {
+              id: args.chatId,
+            },
+            { relations: ["messages"] }
+          );
           if (chat) {
             if (chat.driverId === user.id || chat.passengerId === user.id) {
               return {
